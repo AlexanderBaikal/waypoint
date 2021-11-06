b2990f0b3843b865e626651758470737eaaf3ffc
AlexanderBaikal <abaikaler@mail.ru>
2021-11-06 13:44:27 +0800

    Raise the chunk size warning above the lazy Firebase chunk
    It is the only chunk over the default limit and it is never fetched
    unless Firebase is configured, so the warning was pure noise.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,10 @@ export default defineConfig({
   // No manualChunks on purpose: the Firebase SDK is reached only through
   // dynamic imports (src/data/index.ts, authService), so Rollup already splits
   // it out and a manual grouping would pull it back into the initial bundle.
+  build: {
+    // The one chunk over the default threshold is the Firestore SDK, which is
+    // lazily loaded and never fetched on a fixtures build. Raised so a real
+    // regression in the entry bundle is not lost among expected noise.
+    chunkSizeWarningLimit: 600,
+  },
 });
