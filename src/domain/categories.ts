dd153e7f3b6e858335ba5183d659a7e73e4e0eec
AlexanderBaikal <abaikaler@mail.ru>
2022-01-27 22:08:44 +0800

    Draw the photo placeholder as a real category icon
    Nearly every place has no picture, so the placeholder is the normal
    state and deserves to be drawn rather than defaulted. Each category
    gains an icon — one path on a 24x24 grid, kept as a string in
    CATEGORY_META so the same drawing can go into React and into the raw
    HTML a Leaflet divIcon takes — and the tile carries the category's
    colour as a gradient mixed from that one token, so adding a category
    still means choosing one colour and nothing else.
    
    The glyphs stay on the map pins. At 26px over moving cartography a
    solid geometric mark holds up where a two-pixel line drawing does not,
    and the pin already carries the colour.
    
    Two things this turned up:
    
    - the hero placeholder was grey, not coloured. `.hero` sets a neutral
      background for the photograph it usually holds, and at equal
      specificity the winner is whichever rule comes last in the file. The
      list tile only worked by accident of ordering. Both shapes now name
      themselves alongside `.placeholder`.
    - leisure was drawn as a tree first, which read as a mistake on a
      cinema and on a war memorial. Nothing fits a bucket holding parks,
      gyms, museums and monuments at once, so it is a ticket now — "somewhere
      you go" — and the type is written beside it anyway.

--- src/domain/categories.ts
+++ src/domain/categories.ts
@@ -15,8 +15,14 @@ export type Category = (typeof CATEGORIES)[number];
 
 interface CategoryMeta {
   label: string;
-  /** Single glyph drawn inside the map pin. */
+  /** Single glyph drawn inside the map pin, where an icon would not survive. */
   glyph: string;
+  /**
+   * The category's icon, as the `d` of a single path on a 24×24 grid, stroked
+   * rather than filled. One string rather than a component so the same drawing
+   * can go into React and into the raw HTML string a Leaflet divIcon takes.
+   */
+  path: string;
   /**
    * The category's colour wherever it appears — filter chip, list row, map
    * pin. This is the only saturated colour in the application, and it is a
@@ -37,14 +43,65 @@ interface CategoryMeta {
  * sitting on top of it needs.
  */
 export const CATEGORY_META: Record<Category, CategoryMeta> = {
-  food: { label: "Food", glyph: "◆", colour: "#e2582a" },
-  nightlife: { label: "Nightlife", glyph: "✦", colour: "#8b46c4" },
-  shopping: { label: "Shopping", glyph: "▲", colour: "#0f8a8a" },
-  services: { label: "Services", glyph: "■", colour: "#5b6674" },
-  leisure: { label: "Leisure", glyph: "●", colour: "#2f9e44" },
-  education: { label: "Education", glyph: "▮", colour: "#b8790c" },
-  lodging: { label: "Stay", glyph: "▼", colour: "#d1477a" },
-  other: { label: "Other", glyph: "×", colour: "#7b8290" },
+  food: {
+    label: "Food",
+    glyph: "◆",
+    colour: "#e2582a",
+    // Fork and knife.
+    path: "M7 4v5a2 2 0 0 0 4 0V4M9 11v9M16.5 4c1.6 1.6 1.6 5.4 0 7v9",
+  },
+  nightlife: {
+    label: "Nightlife",
+    glyph: "✦",
+    colour: "#8b46c4",
+    // Cocktail glass.
+    path: "M5 5h14l-7 7zM12 12v7M8.5 19h7",
+  },
+  shopping: {
+    label: "Shopping",
+    glyph: "▲",
+    colour: "#0f8a8a",
+    // Shopping bag.
+    path: "M5.5 8h13l-1 12h-11zM9 8V6a3 3 0 0 1 6 0v2",
+  },
+  services: {
+    label: "Services",
+    glyph: "■",
+    colour: "#5b6674",
+    // Briefcase: banks, post offices and repair shops have no one shape.
+    path: "M4 8h16v11H4zM9 8V5h6v3M4 13h16",
+  },
+  leisure: {
+    label: "Leisure",
+    glyph: "●",
+    colour: "#2f9e44",
+    // A ticket. Nothing fits this bucket — it holds parks, gyms, museums,
+    // cinemas and war memorials — so the mark says "somewhere you go" and
+    // leaves the specifics to the type written beside it. A tree was tried
+    // first and read as a mistake on everything that was not a park.
+    path: "M20 9.5V6H4v3.5a2.5 2.5 0 0 1 0 5V18h16v-3.5a2.5 2.5 0 0 1 0-5z",
+  },
+  education: {
+    label: "Education",
+    glyph: "▮",
+    colour: "#b8790c",
+    // Graduation cap.
+    path: "M12 4 2.5 8.5 12 13l9.5-4.5zM6.5 10.7V16c0 1.7 2.5 3 5.5 3s5.5-1.3 5.5-3v-5.3",
+  },
+  lodging: {
+    label: "Stay",
+    glyph: "▼",
+    colour: "#d1477a",
+    // Bed: headboard, mattress, a rounded foot, and a pillow on top.
+    path: "M3 6v12M3 12h15a3 3 0 0 1 3 3v3M3 18h18M6.5 9.5h4",
+  },
+  other: {
+    label: "Other",
+    glyph: "×",
+    colour: "#7b8290",
+    // Three dots: the bucket for a type we could not name.
+    path: "M6.5 12h.01M12 12h.01M17.5 12h.01",
+  },
 };
 
 // The `type` field is free text — typed by whoever added the place, or derived
--- src/features/places/PlaceImage.tsx
+++ src/features/places/PlaceImage.tsx
@@ -54,7 +54,16 @@ export function PlaceImage({ place, variant }: PlaceImageProps) {
         style={{ "--cat": category.colour } as React.CSSProperties}
         aria-hidden="true"
       >
-        <span>{category.glyph}</span>
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d={category.path} />
+        </svg>
       </div>
     );
   }
